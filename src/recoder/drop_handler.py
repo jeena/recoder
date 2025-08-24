b55f3b44d1a57f17c9738e5a3b903f185edfc1cc
Jeena <hello@jeena.net>
2025-08-24 21:45:23 +0900

    Add possibility to Ctrl-v a path
    Until now it was only possible to drag and drop a path, but now
    that I'm using Hyprland this is not implemented. This patch
    allows me to copy and paste a path either as simple text or
    from a file manager like Nautilus into the app.

--- src/recoder/drop_handler.py
+++ src/recoder/drop_handler.py
@@ -10,12 +10,22 @@ class DropHandler:
         self._accepting = self._compute_accept()
         self.app_state_manager.connect("notify::state", self.on_state_changed)
 
+        # --- Drag & Drop ---
         self.drop_target = Gtk.DropTarget.new(Gio.File, Gdk.DragAction.COPY)
         self.drop_target.connect("enter", self.on_drop_enter)
         self.drop_target.connect("leave", self.on_drop_leave)
         self.drop_target.connect("drop", self.on_drop)
-
         self.w.overlay.add_controller(self.drop_target)
+
+        # --- Clipboard Paste (Ctrl+V) ---
+        self.shortcut_ctrl_v = Gtk.ShortcutController()
+        self.shortcut_ctrl_v.add_shortcut(
+            Gtk.Shortcut.new(
+                Gtk.ShortcutTrigger.parse_string("<Ctrl>V"),
+                Gtk.CallbackAction.new(self.on_paste_clipboard)
+            )
+        )
+        self.w.add_controller(self.shortcut_ctrl_v)
 
     def _compute_accept(self):
         return self.app_state_manager.state not in {
@@ -33,7 +43,7 @@ class DropHandler:
             else:
                 self.w.overlay.remove_controller(self.drop_target)
 
-
+    # ---------------- Drag & Drop ----------------
     def on_drop_enter(self, *_):
         if not self._accepting:
             return False
@@ -47,10 +57,45 @@ class DropHandler:
     def on_drop(self, _, value, __, ___):
         if not self._accepting:
             return False
+        self._start_processing(value)
+        return True
+
+    # ---------------- Clipboard Paste ----------------
+    def on_paste_clipboard(self, *_):
+        if not self._accepting:
+            return True
+
+        clipboard = self.w.get_clipboard()
+        clipboard.read_text_async(None, self._on_clipboard_text_ready)
+        return True
+
+    def _on_clipboard_text_ready(self, clipboard, res):
+        try:
+            text = clipboard.read_text_finish(res)
+        except GLib.Error:
+            return
+
+        if not text:
+            return
+
+        # take the first non-empty line only
+        uri = next((u.strip() for u in text.splitlines() if u.strip()), None)
+        if not uri:
+            return
+
+        if uri.startswith("file://"):
+            gfile = Gio.File.new_for_uri(uri)
+        else:
+            gfile = Gio.File.new_for_path(uri)
+
+        self._start_processing(gfile)
+
+    # ---------------- Shared ----------------
+    def _start_processing(self, value):
         if self.w.drop_hint.get_parent():
             self.w.overlay.remove_overlay(self.w.drop_hint)
         self.w.drop_hint.set_visible(False)
         self.w.progress_bar.set_visible(True)
         self.w.progress_bar.set_fraction(0.0)
         GLib.idle_add(partial(self.w.process_drop_value, value))
-        return True
+
